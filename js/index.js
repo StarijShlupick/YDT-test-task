24e1f72d9f1e91062eaefcc84cfcca36b90bb78b
Maksim Burakou <maxashtet@gmail.com>
2021-09-13 23:35:13 +0300

    feat: add header and burger menu

--- js/index.js
+++ js/index.js
@@ -3,3 +3,19 @@
 //     setting-name: setting-value
 //   });
 // });
+
+const burgerMenu = document.querySelector('.burger-menu');
+const menuLink = document.querySelectorAll('.burger-menu__li');
+const burgerLogo = document.querySelector('.burger');
+const contactUs = document.querySelector('.burger-menu__contact-us')
+const closeBurgerBtn = document.querySelector('.burger-menu__close-btn');
+contactUs.addEventListener('click', toggleBurger);
+burgerLogo.addEventListener('click', toggleBurger);
+menuLink.forEach(link => {
+  link.addEventListener('click', toggleBurger);
+});
+closeBurgerBtn.addEventListener('click', toggleBurger);
+function toggleBurger() {
+  burgerMenu.classList.toggle('--active');
+  document.body.classList.toggle('--scroll-hiden');
+};
